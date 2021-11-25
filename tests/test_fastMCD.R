2eeaf8e0cd24c4601a6051d0e378db0d6216eff3
Frank Ockerman <frank@franks-mbp.home>
2021-11-24 22:09:00 -0500

    Added tests

---
+++ tests/test_fastMCD.R
@@ -0,0 +1,22 @@
+source("../R/fastMCD.R")
+library(testthat)
+
+test_that("Test cov", {
+    set.seed(5500)
+    sigma <- matrix(runif(10^2) * 2 - 1, 10)
+    sigma <- t(sigma) %*% sigma
+    X <- MASS::mvrnorm(1e4, mu = rep(0, 10), Sigma = sigma)
+    X[seq(1, 1e4, 5), ] <- MASS::mvrnorm(2000, mu = rep(3, 10), Sigma = sigma)
+    res <- fastMCD(X)
+    expect_equal(res$cov, sigma, tolerance = 0.2)
+})
+
+test_that("Test center", {
+    set.seed(80124)
+    sigma <- matrix(runif(10^2) * 2 - 1, 10)
+    sigma <- t(sigma) %*% sigma
+    X <- MASS::mvrnorm(1e4, mu <- runif(10), Sigma = sigma)
+    X[seq(1, 1e4, 5), ] <- MASS::mvrnorm(2000, mu = rep(3, 10), Sigma = sigma)
+    res <- fastMCD(X)
+    expect_gte(cor(mu, res$center), 0.9)
+})
